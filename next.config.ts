3e75bad60f11dd6b3b8c7f45b475696bc11157df
Renato <renatoaferreirafilho@gmail.com>
2025-03-11 20:00:43 -0300

    Adicionando assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: "export",
     basePath: "/portfolio", // Substitua pelo nome exato do repositório
+    assetPrefix: "/meu-portfolio/",
     images: {
         unoptimized: true, // GitHub Pages não suporta otimização de imagens Next.js
     },
