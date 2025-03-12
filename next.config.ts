d1526217e8e582c73a25471e8d43cb3a5381f5c5
Renato <renatoaferreirafilho@gmail.com>
2025-03-12 05:14:38 -0300

    Atualizando raiz da aplicacao

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/portfolio", // Substitua pelo nome exato do repositório
     assetPrefix: "./",
     images: {
-        unoptimized: true, // GitHub Pages não suporta otimização de imagens Next.js
+        unoptimized: true,
     },
     trailingSlash: true,
 };
